0049ff0d5ae971fc5d403b6955e861e8ae1856c5
Kaviraj <kaviraj.a@ba.com>
2024-04-27 13:39:11 +0100

    concat,charAt,hashCode

--- String_intro.java
+++ String_intro.java
@@ -4,5 +4,6 @@ public class Main
         String str = "Kavin";
         System.out.println(str.concat(" Raj"));
          System.out.println(str.charAt(2));  //give the value in that index
+           System.out.println(str.hashCode());
     }
 }
