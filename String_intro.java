fb0c1c048c3cbfbabac724b9f25bc5312089e93d
Kaviraj <kaviraj.a@ba.com>
2024-04-27 13:33:28 +0100

    Concat,charAt

---
+++ String_intro.java
@@ -0,0 +1,8 @@
+public class Main
+    {
+    public static void main (String[] args) {
+        String str = "Kavin";
+        System.out.println(str.concat(" Raj"));
+         System.out.println(str.charAt(2));  //give the value in that index
+    }
+}
