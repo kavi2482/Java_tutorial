9fbb203ae1ce1cc8b3c959476dc26ad483a3e417
Kaviraj <kaviraj.a@ba.com>
2024-05-02 06:09:38 +0100

    inheritance

---
+++ inheritance.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Decrease
+{
+    public int sub(int num){
+        return num * num;
+    }
+}
+
+
+class Increase extends Decrease
+{   int num;
+   public Increase(int num)
+   {
+       this.num = num;
+   }
+   public int add(int num1, int num2)
+   {
+       return num * num1 * num2;
+       
+   }
+}
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner ak = new Scanner(System.in);
+        int num = ak.nextInt();
+        Increase obj = new Increase(num);
+        System.out.println(obj.add(10,10));
+        System.out.println(obj.sub(num));
+        
+    }
+}
