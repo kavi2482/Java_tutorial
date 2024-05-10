18d730bce67bd7724d41b838b1856f899c103d32
Kaviraj <kaviraj.a@ba.com>
2024-05-10 07:40:37 +0100

    inner_class

---
+++ inner_class.java
@@ -0,0 +1,26 @@
+class A{
+    int count;
+    public void show()
+    {
+        System.out.println("in Show method");
+    }
+    static class B
+    {
+        public void read()
+        {
+            System.out.println("in read method");
+        }
+    }
+}
+
+
+public class Main{
+    public static void main(String[] a)
+    {
+      A obj = new A();
+      System.out.println(obj.count);
+      obj.show();
+      A.B obj1 = new A.B();    // if static is not mentioned in method => A.B obj1 =obj.new B();
+      obj1.read();
+    }
+}
