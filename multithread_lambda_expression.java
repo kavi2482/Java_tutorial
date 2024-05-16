22f62136b9231878902b79de217640eaf6e57d35
Kaviraj <kaviraj.a@ba.com>
2024-05-16 14:34:19 +0100

    multithreading in lambda expression

---
+++ multithread_lambda_expression.java
@@ -0,0 +1,38 @@
+
+public class Main{
+    public static void main(String[] args)
+    {
+        Runnable obj1 = () ->{
+        for(int i =0;i<100;i++)
+        {
+            System.out.println("Hi");
+            try{
+                Thread.sleep(10);
+            }
+            catch(Exception e)
+            {
+               // e.printStackTrace();
+            }
+        }
+    
+        };
+        Runnable obj2 = () -> {
+            for(int i =0;i<100;i++)
+        {
+            System.out.println("Hello");
+            try{
+                Thread.sleep(10);
+            }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        };
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+        t1.start();
+        t2.start();
+        
+    }
+}
