e62f9cc6a3f37b6137e9571c9792c8ca0eb0217a
Kaviraj <kaviraj.a@ba.com>
2024-05-26 14:04:00 +0100

    Queue with additional methods

---
+++ out/production/Java_tutorial/.idea/Queue.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+           PriorityQueue num = new PriorityQueue();
+           System.out.println(num.offer(10));
+            System.out.println(num);
+           System.out.println(num.add(80));
+            System.out.println(num);
+           System.out.println(num.peek());
+            System.out.println(num);
+           System.out.println(num.element());
+            System.out.println(num);
+            System.out.println(num.remove());
+            System.out.println(num);
+            System.out.println(num.poll());
+
+            System.out.println(num);
+
+        }
+    }
